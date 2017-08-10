fae2f6b02ab1c0dea623a953347aca34ecec8462
Peter Zhang <pengzhangsdcn@gmail.com>
2017-08-10 04:09:11 -0500

    create SFTP user account on AWS Ubuntu
    The script will create a new SFTP user account on AWS Ubuntu: 
    1. You should create sftp-users group and mkdir sftp-root directory in /home. It'll be helpful to manage users and store unique folder under sftp-root. Then you can consider create a root account to control.
    
    
    2. The script will generate a bash file to create a new sFTP user in the same directory with permission 775
    
    3. It'll append new user account in /etc/ssh/sshd_config file to active it.

---
+++ add_sftp_user.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3.6
+import os
+
+userName = input('Who will be added? ')
+
+with open('/home/peter/Downloads/adduser', 'w') as f:
+    f.write('#! /bin/bash')
+    f.write('\nsudo adduser %s' % (userName))
+    f.write('\nsudo usermod -G sftp-users -s /bin/false %s' % (userName))
+    f.write('\nsudo mkdir /home/sftp-root/%s/' %(userName))
+    f.write('\nsudo mkdir /home/sftp-root/%s/Incoming' % (userName))
+    f.write('\nsudo mkdir /home/sftp-root/%s/Outgoing' % (userName))
+    f.write('\nsudo chown %s:%s /home/sftp-root/%s/' % (userName, userName, userName))
+    f.write('\nsudo chmod 755 /home/sftp-root/%s/' % (userName))
+    f.write('\nsudo chmod 755 /home/sftp-root/%s/Incoming' % (userName))
+    f.write('\nsudo chmod 755 /home/sftp-root/%s/Outgoing\n' % (userName))
+
+print('File generated: adduser')
+print('***'*30)
+
+# add 0o as prefix of permission
+os.chmod('./adduser', 0o775)
+print('chmod 775 to file: adduser')
+print('***'*30)
+
+with open('/home/peter/Downloads/adduser') as f:
+    print('adduser shows:')
+    contents=f.read()
+    print(contents)
+print('***'*30)
+
+# update /etc/ssh/sshd_config
+# run it as 'sudo python3.6 add_sftp.py' for privileges issue
+
+with open('/etc/ssh/sshd_config', 'a') as cg:
+    cg.write('\n')
+    cg.write('Match user %s\n' %(userName))
+    cg.write('\tChrootDirectory /home/sftp-root/%s' %(userName))
+
+print('sshd_config file updated.')
+print('***'*30)
+
+with open('/etc/ssh/sshd_config') as cg:
+    print('sshd_config shows:')
+    contents=cg.read()
+    print(contents)
